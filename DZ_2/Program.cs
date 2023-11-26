f8b05c149efb117b1edafd71a83a0c9bb3a34e81
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-11-26 12:30:42 +0300

    DZ_2 задача 1

---
+++ DZ_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int user_input = Convert.ToInt32(Console.ReadLine());
+if (user_input % 7 == 0 && user_input % 23 == 0)
+{
+    System.Console.WriteLine($"{user_input} кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine($"{user_input} не кратно 7 и 23");
+}
