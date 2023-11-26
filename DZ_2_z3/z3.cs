39f2d9c740090b39441271057407539513f8f819
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-11-26 16:35:56 +0300

    DZ_2 задание 3

---
+++ DZ_2_z3/z3.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число из отрезка [10, 99]: ");
+int user_input = Convert.ToInt32(Console.ReadLine());
+int num_1 = user_input / 10;
+int num_2 = user_input % 10;
+if (num_1 > num_2)
+    System.Console.WriteLine(num_1);
+    else {
+System.Console.WriteLine(num_2);
+    }
