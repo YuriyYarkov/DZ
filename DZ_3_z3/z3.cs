e74065b41fd86fae6bcb07138a250b7bf2acbdc3
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-12-02 23:55:12 +0300

    DZ_3 задача 3

---
+++ DZ_3_z3/z3.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая перевернёт одномерный массив 
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Исходный массив:");
+        PrintArray(arr);
+        ReverseArray(arr);
+        Console.WriteLine("\nМассив после переворота:");
+        PrintArray(arr);
+    }
+    static void ReverseArray(int[] arr)
+    {
+        int start = 0;
+        int end = arr.Length - 1;
+
+        while (start < end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+    static void PrintArray(int[] arr)
+    {
+        foreach (int e in arr)
+        {
+            Console.Write(e + " ");
+        }
+    }
+}
+
+
