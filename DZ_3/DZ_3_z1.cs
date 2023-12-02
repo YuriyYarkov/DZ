9bf00092b8e82379dcc4b4a6b8a6bbe63860d20c
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-12-02 22:40:27 +0300

    DZ_3 задача 1

---
+++ DZ_3/DZ_3_z1.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+//Программа завершается при вводе символа ‘q’ или при вводе числа, 
+//сумма цифр которого чётная.
+while (true)
+{
+    Console.Write("Введите целое число или 'q' для выхода из программы: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        Console.WriteLine("Вы ввели 'q', программа завершена");
+        break;
+    }
+
+    if (int.TryParse(input, out int number))
+    {
+        if (IsEvenDigitSum(number))
+        {
+            Console.WriteLine($"Сумма цифр числа {number} четная. Программа завершена.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Сумма цифр числа {number} нечетная.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ошибка: Введенное значение не является целым числом.");
+    }
+}
+
+bool IsEvenDigitSum(int number)
+{
+    int digitSum = 0;
+
+    while (number != 0)
+    {
+        digitSum += number % 10;
+        number /= 10;
+    }
+
+    return digitSum % 2 == 0;
+}
+
